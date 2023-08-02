68ea9f25869015fde370bf328077e30c32f27904
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2023-08-01 21:47:16 -0300

    Habit Tracker

--- HabitTracker/HabitTracker/NewHabitType.swift
+++ HabitTracker/HabitTracker/NewHabitType.swift
@@ -8,13 +8,24 @@
 import Foundation
 
 class NewHabitType: ObservableObject {
-    @Published var habit = [HabitType]() {
+    @Published var habits = [HabitType]() {
         didSet {
             let encoder = JSONEncoder()
-            if let encoderItem = try? encoder.encode(habit) {
+            if let encoderItem = try? encoder.encode(habits) {
                 UserDefaults.standard.set(encoderItem, forKey: "Habit Item")
             }
         }
     }
     
+    init() {
+        let decoder = JSONDecoder()
+        
+        if let saveHabits = UserDefaults.standard.data(forKey: "Habit Item") {
+            if let decoderHabit = try? decoder.decode([HabitType].self, from: saveHabits) {
+                habits = decoderHabit
+                return
+            }
+        }
+        habits = []
+    }
 }
