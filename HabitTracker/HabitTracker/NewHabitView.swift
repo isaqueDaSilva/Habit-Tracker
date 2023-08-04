46acdcda51aefba3fc20c550e1e94e91f0f04dc9
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2023-08-03 23:46:16 -0300

    Add new habit view

--- HabitTracker/HabitTracker/NewHabitView.swift
+++ HabitTracker/HabitTracker/NewHabitView.swift
@@ -10,19 +10,40 @@ import SwiftUI
 struct NewHabitView: View {
     @ObservedObject var viewModel = HabitViewModel()
     
-    var nome: String
+    @State private var name: String = ""
+    @State private var description: String? = ""
+    @State private var icone: String = ""
+    @State private var repeatIn: Repeat = .oneTime
+    @State private var priority: Priority = .high
+    
     var body: some View {
         NavigationView {
             List {
-                
+                Section("Details:") {
+                    TextField("Name:", text: $name)
+                    
+                    Picker("Priority", selection: $priority) {
+                        ForEach(Priority.allCases, id: \.self) {
+                            Text($0.rawValue)
+                        }
+                    }
+                    
+                    Picker("Repeat", selection: $repeatIn) {
+                        ForEach(Repeat.allCases, id: \.self) {
+                            Text("\($0.rawValue)x a Week")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
             }
-            .navigationTitle(nome)
+            .navigationTitle("New Habit")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
 
 struct NewHabitView_Previews: PreviewProvider {
     static var previews: some View {
-        NewHabitView(nome: "")
+        NewHabitView()
     }
 }
